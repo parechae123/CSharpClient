d6f7c156ce95ab72270454348e5be1e68da26bdf
heoyoon <parechae@gmail.com>
2025-01-14 03:13:11 +0900

    feat : 프로젝트 세팅 및 메인프로그램 스크립트 추가

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Client
+using System.Net.Sockets;
+using System.Text;
+class Program
+{
+
+    static async Task Main()
+    {
+        try
+        {
+            using var client = new TcpClient("127.0.0.1", 7777);
+            using var stream = client.GetStream();
+            Sequence sequence = new Sequence();
+            await sequence.Login(stream);
+            _= Task.Run(()=>sequence.ReceiveMessage(stream));
+            await sequence.Chatting(stream);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"오류 발생: {ex.Message}");
+        }
+    }
+
+}
