350a57dac0e4584e40abd8b65d7f8a606c3cd6f6
heoyoon <parechae@gmail.com>
2025-01-15 20:19:28 +0900

    feat : protocBuf 추가, 객체 주고받는 스크립트 작성 중

---
+++ Client/ProtocTest.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClient.Client
+{
+    public class ProtocTest
+    {
+        public async Task Process(TcpClient client, NetworkStream stream)
+        {
+
+            Login(stream,client);
+        }
+        public async void Login(NetworkStream stream,TcpClient client)
+        {
+            byte[] buff = new byte[1024];
+            byte[] data;
+            string message;
+            int buffRead;
+            
+            while (true)
+            {
+                buffRead = stream.Read(buff, 0, buff.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(buff,0,buffRead));
+                message = Console.ReadLine() ?? ""; //readLine으로 string 받아와서
+                data = Encoding.UTF8.GetBytes(message);//byte 배열로 바꿈
+
+                //닉네임 객체 보냄
+                await stream.WriteAsync(data, 0, data.Length);//바이트 배열을 전송
+                User tempUser = ProtoBuf.Serializer.Deserialize<User>(stream);
+                Console.WriteLine(tempUser.UserID??string.Empty);
+                
+            }
+            Console.WriteLine("클라에서 탈출");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿//Client
+using CSharpClient.Client;
 using System.Net.Sockets;
 using System.Text;
 class Program
 {
 
+    static async Task Main()
+    {
+        try
+        {
+            ProtocTest sequence = new ProtocTest();
+            TcpClient tcpClient = new TcpClient("127.0.0.1", 7777);
+            NetworkStream stream = tcpClient.GetStream();
+            await sequence.Process(tcpClient,stream);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"오류 발생: {ex.Message}");
+        }
+    }
+    /*
     static async Task Main()
     {
         try
@@ -21,5 +38,5 @@ class Program
             Console.WriteLine($"오류 발생: {ex.Message}");
         }
     }
-
+*/
 }
